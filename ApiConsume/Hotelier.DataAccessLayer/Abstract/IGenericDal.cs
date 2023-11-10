9a435a10c4d27a9899891d6834d040b20b7f9fc1
CihanDikkaya <cihandikkaya09@hotmail.com>
2023-11-10 18:13:32 +0300

    Interface'ler yazıldı.

---
+++ ApiConsume/Hotelier.DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotelier.DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T> where T : class
+    {
+        void Insert(T t);
+        void Update(T t);
+        void Delete(T t);
+        List<T> GetList();
+        T GetByID(int id);
+
+    }
+}
