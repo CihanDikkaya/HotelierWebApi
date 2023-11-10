305e744137966d865150486b828808353080388b
CihanDikkaya <cihandikkaya09@hotmail.com>
2023-11-10 18:03:07 +0300

    sınıflarımız veri tabanına yansıtıldı.

---
+++ ApiConsume/Hotelier.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,27 @@
+﻿using Hotelier.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotelier.DataAccessLayer.Concrete
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=CIHAN; initial catalog=ApiDB; integrated security=True; TrustServerCertificate=True;");
+        }
+
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Staff> Staff { get; set; }
+        public DbSet<Subscribe> Subscribes { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+
+
+
+    }
+}
