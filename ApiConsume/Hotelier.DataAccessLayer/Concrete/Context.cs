f999b70c8f216ce4d59710485f5f8d9540b737ed
CihanDikkaya <cihandikkaya09@hotmail.com>
2024-02-15 01:31:52 +0300

    MSSQL bağlantısı yapıldı.

---
+++ ApiConsume/Hotelier.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,25 @@
+﻿using Hotelier.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotelier.DataAccessLayer.Concrete
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=CIHAN;initial catalog=ApiDb; integrated security=True");
+
+
+        }
+        public DbSet<Room> Rooms  { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Staff> Staff { get; set; }
+        public DbSet<Subscribe> Subscribes { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+    }
+}
