ff2a3b5cce47bc118786ba60bca0abfd8a466aae
CihanDikkaya <cihandikkaya09@hotmail.com>
2024-04-18 11:31:37 +0300

    Otel Tanıtım videosu eklendi.

--- Frontend/Hotelier.WebUI/ViewComponents/Default/_RoomPartial.cs
+++ Frontend/Hotelier.WebUI/ViewComponents/Default/_RoomPartial.cs
@@ -1,10 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Hotelier.WebUI.DTOS.RoomDTO;
+using Hotelier.WebUI.DTOS.ServiceDTO;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
 namespace Hotelier.WebUI.ViewComponents.Default
 {
     public class _RoomPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _RoomPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:61440/api/Room");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsdata = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultRoomDTO>>(jsdata);
+                return View(values);
+            }
             return View();
         }
     }
