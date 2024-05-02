2a5e3495cdc8bc75b9f1559acffa310c6eb559e3
CihanDikkaya <cihandikkaya09@hotmail.com>
2024-05-02 10:52:25 +0300

    Rezervasyon Yönetici Paneli oluşturuldu

---
+++ Frontend/Hotelier.WebUI/Controllers/BookingAdminController.cs
@@ -0,0 +1,34 @@
+﻿using Hotelier.WebUI.DTOS.BookingDTO;
+using Hotelier.WebUI.DTOS.ServiceDTO;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Hotelier.WebUI.Controllers
+{
+    public class BookingAdminController : Controller
+    {
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public BookingAdminController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:61440/api/Booking");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsdata = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBookingDTO>>(jsdata);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
