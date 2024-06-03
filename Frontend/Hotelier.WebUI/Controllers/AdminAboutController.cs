07dce837992ef37e74cf1b28906237d628f93a0b
CihanDikkaya <cihandikkaya09@hotmail.com>
2024-06-03 12:08:18 +0300

    Admin panelinde hakkımızda sayfası güncelleme işlemi.

---
+++ Frontend/Hotelier.WebUI/Controllers/AdminAboutController.cs
@@ -0,0 +1,66 @@
+﻿using Hotelier.WebUI.DTOS.AboutDTO;
+using Hotelier.WebUI.DTOS.BookingDTO;
+using Hotelier.WebUI.Models.Staff;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotelier.WebUI.Controllers
+{
+    public class AdminAboutController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminAboutController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:61440/api/About");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsdata = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultAboutDTO>>(jsdata);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateAbout(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"http://localhost:61440/api/About/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsdata = await responseMessage.Content.ReadAsStringAsync();
+                var val = JsonConvert.DeserializeObject<UpdateAboutDTO>(jsdata);
+                return View(val);
+            }
+            return View();
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateAbout(UpdateAboutDTO model)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsdata = JsonConvert.SerializeObject(model);
+            StringContent stringContent = new StringContent(jsdata, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("http://localhost:61440/api/About/", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+
+    }
+}
