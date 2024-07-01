14f750fd5c4702c128b7c05ba9e485b1bad7a2e2
CihanDikkaya <cihandikkaya09@hotmail.com>
2024-07-01 09:43:49 +0300

    Admin Panelinde mesaj işlemi başladı.

---
+++ Frontend/Hotelier.WebUI/Controllers/AdminContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hotelier.WebUI.Controllers
+{
+    public class AdminContactController : Controller
+    {
+        public IActionResult Inbox()
+        {
+            return View();
+        }
+    }
+}
